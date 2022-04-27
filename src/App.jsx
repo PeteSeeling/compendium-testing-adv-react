b48eeba76d1820a1a1218e8058d00abaed33fbf8
Pete Seeling <peteseeling@gmail.com>
2022-04-26 18:48:04 -0600

    returning data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import FuturamaList from "./components/futuramalist";
+import FuturamaList from './components/futuramalist';
+
+import { Link, Route, Switch } from 'react-router-dom'
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+  <><h1>Hello World</h1><FuturamaList /></>
+  )
 }
--- src/components/FuturamaList.js
+++ src/components/FuturamaList.js
@@ -1,5 +1,45 @@
+
 import { useEffect, useState } from 'react';
 
+
 export default function FuturamaList(){
+ const [characters, setCharacters] = useState([]);
+const [loading, setLoading] = useState(true);
+
+
+
+useEffect(() => {
+async function getCharacters() {
+const res = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
+console.log(await res.json());
+
+const characters = result.results;
+const resultCount = result.length;
+
+setCharacters(characters)
+console.log(characters, resultCount)
+
+}
+getCharacters();
+setLoading(false);
+
+},[])
+// if (loading){
+
+return(
+    <>
+    <h3>Characters of FuturamaList</h3>
+        {
+        characters.map((character, i) => {
+            return (<div>
+              <h2>{characters.Name}Name</h2> 
+              
+               </div>
+            )
+        })}
     
+    
+    </>
+)
+
 }
